14c3421a39bb1a6d90108ab951d73a65f5df4249
FiendsTear <94nerevar@gmail.com>
2023-02-15 14:57:58 +0400

    add logging to ws server

--- src/server/websockets/wsServer.ts
+++ src/server/websockets/wsServer.ts
@@ -1,30 +1,26 @@
 import ws from "ws";
-import { createWSContext } from '../trpc';
+import { createWSContext } from "../trpc";
 import { AppRouter, appRouter } from "../mainRouter";
-import { applyWSSHandler } from '@trpc/server/adapters/ws';
+import { applyWSSHandler } from "@trpc/server/adapters/ws";
 
 const wss = new ws.Server({
   port: 3100,
 });
 
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
-import SuperJSON from "superjson";
-const client = createTRPCProxyClient<AppRouter>({
-  transformer: SuperJSON,
-  links: [
-    httpBatchLink({
-      url: 'http://localhost:3001/api/trpc',
-    }),
-  ],
+const handler = applyWSSHandler({
+  wss,
+  router: appRouter,
+  createContext: createWSContext,
 });
 
-const handler = applyWSSHandler({ wss, router: appRouter, createContext: createWSContext });
 wss.on("connection", (ws) => {
   console.log(`➕➕ Connection (${wss.clients.size})`);
   ws.once("close", (socket, reason) => {
     console.log(`➖➖ Connection (${wss.clients.size})`);
   });
 });
+
+wss.on("error", (err) => console.error(err));
 
 console.log("✅ WebSocket Server listening on ws://localhost:3100");
 process.on("SIGTERM", () => {
