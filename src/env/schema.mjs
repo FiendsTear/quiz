fa890dcdf2d79771f8e3d75be5d2e529ee1bcfe9
FiendsTear <94nerevar@gmail.com>
2023-05-10 16:51:02 +0400

    remove vk vars

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -19,8 +19,8 @@ export const serverSchema = z.object({
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
     process.env.VERCEL ? z.string() : z.string().url(),
   ),
-  VK_CLIENT_ID: z.string(),
-  VK_CLIENT_SECRET: z.string(),
+//   VK_CLIENT_ID: z.string(),
+//   VK_CLIENT_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string()
 });
@@ -35,8 +35,8 @@ export const serverEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  VK_CLIENT_ID: process.env.VK_CLIENT_ID,
-  VK_CLIENT_SECRET: process.env.VK_CLIENT_SECRET,
+//   VK_CLIENT_ID: process.env.VK_CLIENT_ID,
+//   VK_CLIENT_SECRET: process.env.VK_CLIENT_SECRET,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
 };
